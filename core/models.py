11263cc18665c5ddd0a1baf0e4646072adfca780
sanss021 <smlmonteiroum@gmail.com>
2020-08-11 22:09:06 -0300

    consertando o commit de configuração de tela de exclusão e edição de dados

--- core/models.py
+++ core/models.py
@@ -17,4 +17,7 @@ class Evento(models.Model):
         return self.titulo
 
     def get_data_evento(self):
-        return self.data_evento.strftime('%d/%n/%Y %H: %M')
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
--- core/views.py
+++ core/views.py
@@ -12,20 +12,22 @@ from django.contrib import messages
 def login_user(request):
     return render(request, 'login.html')
 
+
 def logout_user(request):
     logout(request)
     return redirect('/')
+
 
 def submit_login(request):
     if request.POST:
         username = request.POST.get('username')
         password = request.POST.get('password')
-        usuario = authenticate(username = username, password = password)
+        usuario = authenticate(username=username, password=password)
         if usuario is not None:
             login(request, usuario)
             return redirect('/')
         else:
-            messages.error(request, "Usuário ou senha inválido" )
+            messages.error(request, "Usuário ou senha inválido")
     return redirect('/')
 
 
@@ -33,12 +35,18 @@ def submit_login(request):
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
-    dados = {'eventos' :evento}
+    dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
+
 @login_required(login_url='/login/')
-def evento (request):
-    return render(request, 'evento.html')
+def evento(request):
+    id_evento = request.GET.get('id')
+    dados = {}
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html',dados)
+
 
 @login_required(login_url='/login/')
 def submit_evento(request):
@@ -46,12 +54,26 @@ def submit_evento(request):
         titulo = request.POST.get('titulo')
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
-        usuario= request.user
-        Evento.objects.create(titulo=titulo,
-                              data_evento=data_evento,
-                              descricao=descricao,
-                              usuario=usuario)
+        usuario = request.user
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            Evento.objects.filter(id=id_evento).update(titulo=titulo,
+                                                       data_evento=data_evento,
+                                                       descricao=descricao)
+
+        else:
+            Evento.objects.create(titulo=titulo,
+                                  data_evento=data_evento,
+                                  descricao=descricao,
+                                  usuario=usuario)
 
     return redirect('/')
 
 
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
+    return redirect('/')
